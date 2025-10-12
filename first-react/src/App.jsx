00a26541f6b131b4deeb87da3162f8f07500a67e
Shubhanshu <shbhansh66@gmail.com>
2025-10-12 20:08:08 +0530

    f react  commit

---
+++ first-react/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<>
+<div>
+  <h1>Hi This is my first app</h1>
+</div>
+
+<div>
+  <h1>
+    First
+  </h1>
+</div>
+</>
+  )
+}
+
+export default App
